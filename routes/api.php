b7485c3846a5f33f63d7fd0764820b11b2440234
Aldo Saman <aldosaman711@gmail.com>
2023-11-29 23:24:56 +0700

    OlahragaController Done

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BakatMinatController;
+use App\Http\Controllers\OlahragaController;
 
 
 /*
@@ -28,4 +29,9 @@ Route::post("auth/ceking",[UserController::class, "ceking"]);
 //pdbabm
 Route::post("v1/pdbabm/simpan",[BakatMinatController::class, "store"]);
 
-
+// Olahraga
+Route::get("v1/olahraga/index", [OlahragaController::class, "index"]);
+Route::post("v1/olahraga/store", [OlahragaController::class, "store"]);
+Route::get("v1/olahraga/show/{id}", [OlahragaController::class, "show"]);
+Route::post("v1/olahraga/update/{id}", [OlahragaController::class, "update"]);
+Route::delete("v1/olahraga/destroy/{id}", [OlahragaController::class, "destroy"]);
